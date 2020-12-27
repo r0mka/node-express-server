974f7c4c026b603d31b66a761a6d9c57753e5f45
r0mka <romanliveshere@gmail.com>
2020-12-26 19:00:45 -0800

    Add nodemon

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from server');
+});
+
+app.get('/info', (req, res) => {
+  res.send('Info route');
+});
+
+app.listen(process.env.PORT || PORT, () =>
+  console.log(`Server running on port: `, PORT)
+);
