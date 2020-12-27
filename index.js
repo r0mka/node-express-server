3916aacae16863cecf09a8a6e15a22622f8723a8
r0mka <romanliveshere@gmail.com>
2020-12-26 20:58:59 -0800

    Install eslint-config-prettier and eslint-plugin-prettier

--- index.js
+++ index.js
@@ -12,6 +12,4 @@ app.get('/info', (req, res) => {
   res.send('Info route');
 });
 
-app.listen(process.env.PORT || PORT, () =>
-  console.log('Server running on port: ', PORT)
-);
+app.listen(process.env.PORT || PORT, () => console.log('Server running on port: ', PORT));
