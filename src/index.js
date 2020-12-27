b821be784e0a071a686ae358ea38e7f9a02e254b
r0mka <romanliveshere@gmail.com>
2020-12-27 00:16:11 -0800

    Parse body

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import { home } from './home';
 import { info } from './info';
 import { errorHandler } from './errorHandler';
 import { logger } from './logger';
+import { register } from './register';
 
+const PORT = 5000;
 const app = express();
 app.use(express.json());
 logger(app);
 
-const PORT = 5000;
-
 app.get('/', home);
-
 app.get('/info', info);
+app.post('/register', register);
 
 errorHandler(app);
 
